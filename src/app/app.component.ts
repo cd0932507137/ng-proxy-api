e83ee8c6aa20ef7e6166c39c78cfb35af389ad13
cd0932507137 <cd0932507137@yahoo.com.tw>
2019-02-26 22:14:39 +0800

    RESTful APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo1';
+  data: any;
+  constructor(private http: HttpClient) {
+	}
+  
+  ngOnInit() {
+    let url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
+    this.data = this.http.get(url).pipe(map(data => {
+      return data;
+    }));
+  }
 }
